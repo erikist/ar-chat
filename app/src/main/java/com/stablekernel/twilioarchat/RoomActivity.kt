e6e5c006fa5f273a3134caa6a85164b2323cc873
Erik Rahtjen <erik.rahtjen@stablekernel.com>
2018-05-21 18:58:36 -0400

    WIP

--- app/src/main/java/com/stablekernel/twilioarchat/RoomActivity.kt
+++ app/src/main/java/com/stablekernel/twilioarchat/RoomActivity.kt
@@ -1,12 +1,12 @@
 package com.stablekernel.twilioarchat
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class RoomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        DatabindingUtil
+        DataBindingUtil.setContentView<>()
     }
 }

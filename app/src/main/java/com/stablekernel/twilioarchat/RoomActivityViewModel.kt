82f77372eaf4d938310e4ab2d02cf4078b556354
Erik Rahtjen <erik.rahtjen@stablekernel.com>
2018-05-21 19:34:50 -0400

    Viewmodel commit

---
+++ app/src/main/java/com/stablekernel/twilioarchat/RoomActivityViewModel.kt
@@ -0,0 +1,9 @@
+package com.stablekernel.twilioarchat
+
+import android.databinding.BaseObservable
+
+
+class RoomActivityViewModel: BaseObservable() {
+    var room: String = ""
+    var username: String = ""
+}
